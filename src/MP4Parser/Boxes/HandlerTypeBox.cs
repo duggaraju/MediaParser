c6fd09162cd26588a14956a742225483c8788717
Oleg Kosmakov <1533952+kosmakoff@users.noreply.github.com>
2024-01-25 21:43:01 +0100

    Handle the unknown string encoding in HDLR component type name

--- src/MP4Parser/Boxes/HandlerTypeBox.cs
+++ src/MP4Parser/Boxes/HandlerTypeBox.cs
@@ -1,8 +1,14 @@
-﻿namespace Media.ISO.Boxes
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Media.ISO.Boxes
 {
     [BoxType(BoxType.HandlerTypeBox)]
     public class HandlerTypeBox : Box
     {
+        private bool _isComponentTypeNamePascalString;
+        
         public HandlerTypeBox() : base(BoxType.HandlerTypeBox)
         {
         }
@@ -31,7 +37,36 @@
             Manufacturer = reader.ReadInt32();
             Reserved = reader.ReadInt32();
             ReservedMask = reader.ReadInt32();
-            ComponentTypeName = reader.ReadString();
+
+            var remainingLength = (int)Size - HeaderSize - 24;
+
+            // This part tries to handle string with unknown encoding
+            // It may be Pascal string (length as 0th byte), or it may be C string (zero terminated)
+            Span<byte> componentTypeNameSpan = stackalloc byte[remainingLength];
+            int offset = 0;
+
+            do
+            {
+                var readBytes = reader.BaseStream.Read(componentTypeNameSpan[offset..]);
+                offset += readBytes;
+            } while (offset < remainingLength);
+
+            if (componentTypeNameSpan[^1] == 0)
+            {
+                // it is C string
+                _isComponentTypeNamePascalString = false;
+                ComponentTypeName = Encoding.ASCII.GetString(componentTypeNameSpan[..^1]);
+            }
+            else if (componentTypeNameSpan[0] == remainingLength - 1)
+            {
+                // it is Pascal string
+                _isComponentTypeNamePascalString = true;
+                ComponentTypeName = Encoding.ASCII.GetString(componentTypeNameSpan[1..]);
+            }
+            else
+            {
+                throw new InvalidDataException("Component type name field is neither C not Pascal string");
+            }
         }
 
         protected override void WriteBoxContent(BoxWriter writer)
@@ -42,7 +77,20 @@
             writer.WriteInt32(Manufacturer);
             writer.WriteInt32(Reserved);
             writer.WriteInt32(ReservedMask);
-            writer.WriteString(ComponentTypeName);
+
+            Span<byte> componentTypeName = stackalloc byte[ComponentTypeName.Length];
+            Encoding.ASCII.GetBytes(ComponentTypeName.AsSpan(), componentTypeName);
+            
+            if (_isComponentTypeNamePascalString)
+            {
+                writer.BaseStream.WriteByte((byte)ComponentTypeName.Length);
+                writer.BaseStream.Write(componentTypeName);
+            }
+            else
+            {
+                writer.BaseStream.Write(componentTypeName);
+                writer.BaseStream.WriteByte(0);
+            }
         }
     }
 }
